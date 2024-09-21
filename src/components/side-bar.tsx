bba739bbe43771c75ffa267436a9d52785f303a5
jeonghoyang12 <wjdwjdgh6998@gmail.com>
2024-09-21 01:03:07 -0400

    refactor: add icons in sidebar

--- src/components/side-bar.tsx
+++ src/components/side-bar.tsx
@@ -1,31 +1,38 @@
-import React from "react"
-import Image from "next/image"
+import React from 'react'
 
 export default function Sidebar() {
   return (
-    <aside className="text-white w-16 flex flex-col items-center py-4 h-screen">
-      <div className="space-y-6">
-        <SidebarIcon icon="/firefox.svg" alt="Firefox" />
-        <SidebarIcon icon="/folder.svg" alt="My Favorites" />
-        <SidebarIcon icon="/home.svg" alt="Home" />
-        <SidebarIcon icon="/folder-generic.svg" alt="Generic Folder" />
-        <SidebarIcon icon="/target.svg" alt="Target" />
-        <SidebarIcon icon="/document.svg" alt="Document" />
-        <SidebarIcon icon="/help.svg" alt="Help" />
-        <SidebarIcon icon="/terminal.svg" alt="Terminal" />
-        <SidebarIcon icon="/recycle.svg" alt="Recycle Bin" />
+    <aside className="text-white w-16 flex flex-col items-center justify-center h-screen">
+      <div className="space-y-4 py-2">
+        <SidebarIcon>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+            <polyline points="9 22 9 12 15 12 15 22"></polyline>
+          </svg>
+        </SidebarIcon>
+        <SidebarIcon>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+          </svg>
+        </SidebarIcon>
+        <SidebarIcon>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+            <polyline points="14 2 14 8 20 8"></polyline>
+            <line x1="16" y1="13" x2="8" y2="13"></line>
+            <line x1="16" y1="17" x2="8" y2="17"></line>
+            <polyline points="10 9 9 9 8 9"></polyline>
+          </svg>
+        </SidebarIcon>
       </div>
     </aside>
   )
 }
 
-const SidebarIcon: React.FC<{ icon: string; alt: string }> = ({
-  icon,
-  alt,
-}) => {
+const SidebarIcon: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <button className="p-2 rounded-md hover:bg-purple-800 transition-colors">
-      <Image src={icon} width={24} height={24} alt={alt} />
+    <button className="p-2 rounded-md hover:bg-neutral-800 transition-colors">
+      {children}
     </button>
   )
 }
